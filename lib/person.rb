5a199a95a34e7c0883d07d1112a19163b2415205
Will <willchesson@gmail.com>
2022-06-21 12:03:09 -0400

    commit tree b23d35d971cdc5223f05621482436258f1298655

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,91 @@
-# your code goes here
+require "pry"
+
+class Person 
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = value.clamp(0, 10)
+    end
+
+    def hygiene=(value)
+        @hygiene = value.clamp(0, 10)
+    end
+
+    def clean? 
+        if hygiene > 7
+            true
+        else
+            false
+        end
+        #self.hygiene > 7
+        #the one line above will return T or F
+    end
+
+    def happy? 
+        if happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins" 
+        #returns this string if write it like this. Puts prints to terminal
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    # def start_conversation(friend, topic)
+    #     #could use case
+    #     if topic == "politics"
+    #         #[self, friend].each {|person| person.happiness -= 2}
+    #         self.happiness -= 2
+    #         friend.happiness -= 2
+    #         "blah blah partisan blah lobbyist"
+    #     elsif topic == "weather"
+    #         self.happiness += 1
+    #         friend.happiness += 1
+    #         "blah blah sun blah rain"
+    #     else
+    #         "blah blah blah blah blah"
+    #     end
+    # end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            [self, friend].each{|person| person.happiness -= 2}
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            [self, friend].each{|person| person.happiness += 1}
+            "blah blah sun blah rain"
+        else 
+            "blah blah blah blah blah"
+        end
+    end
+end
